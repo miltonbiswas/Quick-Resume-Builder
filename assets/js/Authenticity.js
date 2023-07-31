96d64998d9692e4d6a02c2afec7bbd38179808e1
Milton Biswas <miltonbiswasphotography@hotmail.com>
2023-08-01 02:27:10 +0530

    Main
    Push it to master  branch

---
+++ assets/js/Authenticity.js
@@ -0,0 +1,8 @@
+window.confirm("Welcome! This project is developed by Milton Biswas. For the source code you can visit GitHub. Please give it a star.");
+
+if (confirm("Let's explore.")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+  
